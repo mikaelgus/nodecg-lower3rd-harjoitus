aa2892903d82cada22aa012556801225079de230
Mikael Gustafsson <mikaelgustafsson@Mikaels-MacBook-Pro.local>
2022-10-12 15:37:24 +0300

    first commit

---
+++ extension/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+module.exports = function (nodecg) {
+  nodecg.Replicant("speakerReplicant", { defaultValue: "Ukko Hukkanen" });
+  nodecg.Replicant("roleReplicant", { defaultValue: "Masiinahenkilö" });
+};
